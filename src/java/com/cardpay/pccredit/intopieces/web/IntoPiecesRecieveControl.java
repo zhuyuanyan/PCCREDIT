33c7020aaf73217de9fe458992a8b0c801cf1794
qkhj <qk_hejia@163.com>
2015-01-29 14:25:35 +0800

    【modify】

---
+++ src/java/com/cardpay/pccredit/intopieces/web/IntoPiecesRecieveControl.java
@@ -0,0 +1,206 @@
+package com.cardpay.pccredit.intopieces.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cardpay.pccredit.customer.constant.CustomerInforConstant;
+import com.cardpay.pccredit.customer.constant.WfProcessInfoType;
+import com.cardpay.pccredit.customer.dao.CustomerInforDao;
+import com.cardpay.pccredit.customer.dao.comdao.CustomerInforCommDao;
+import com.cardpay.pccredit.customer.filter.CustomerInforFilter;
+import com.cardpay.pccredit.customer.model.CustomerCareersInformation;
+import com.cardpay.pccredit.customer.model.CustomerInfor;
+import com.cardpay.pccredit.customer.service.CustomerInforService;
+import com.cardpay.pccredit.datapri.constant.DataPriConstants;
+import com.cardpay.pccredit.datapri.service.DataAccessSqlService;
+import com.cardpay.pccredit.intopieces.constant.ApplicationStatusEnum;
+import com.cardpay.pccredit.intopieces.constant.CardStatus;
+import com.cardpay.pccredit.intopieces.constant.Constant;
+import com.cardpay.pccredit.intopieces.constant.IntoPiecesException;
+import com.cardpay.pccredit.intopieces.filter.CustomerApplicationProcessFilter;
+import com.cardpay.pccredit.intopieces.filter.IntoPiecesFilter;
+import com.cardpay.pccredit.intopieces.filter.MakeCardFilter;
+import com.cardpay.pccredit.intopieces.model.CustomerAccountData;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationCom;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationContact;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationContactVo;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationGuarantor;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationGuarantorVo;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationInfo;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationOther;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationProcess;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationRecom;
+import com.cardpay.pccredit.intopieces.model.CustomerApplicationRecomVo;
+import com.cardpay.pccredit.intopieces.model.IntoPieces;
+import com.cardpay.pccredit.intopieces.model.MakeCard;
+import com.cardpay.pccredit.intopieces.service.CustomerApplicationIntopieceWaitService;
+import com.cardpay.pccredit.intopieces.service.CustomerApplicationProcessService;
+import com.cardpay.pccredit.intopieces.service.IntoPiecesService;
+import com.cardpay.pccredit.product.filter.ProductFilter;
+import com.cardpay.pccredit.product.model.AddressAccessories;
+import com.cardpay.pccredit.product.model.AppendixDict;
+import com.cardpay.pccredit.product.model.ProductAttribute;
+import com.cardpay.pccredit.product.service.ProductService;
+import com.cardpay.pccredit.system.constants.NodeAuditTypeEnum;
+import com.cardpay.pccredit.system.constants.YesNoEnum;
+import com.cardpay.pccredit.system.model.NodeAudit;
+import com.cardpay.pccredit.system.model.NodeControl;
+import com.cardpay.pccredit.system.service.NodeAuditService;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_ADDR;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_DBXX;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_DCSC;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_HKSZ;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_KPMX;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_QTXYKXX;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_SQED;
+import com.cardpay.pccredit.xm_appln.model.XM_APPLN_TJINFO;
+import com.cardpay.pccredit.xm_appln.service.XM_APPLN_Service;
+import com.cardpay.pccredit.xm_appln.web.XM_APPLN_Controller;
+import com.cardpay.workflow.models.WfProcessInfo;
+import com.cardpay.workflow.models.WfStatusInfo;
+import com.cardpay.workflow.models.WfStatusResult;
+import com.cardpay.workflow.service.ProcessService;
+import com.wicresoft.jrad.base.auth.IUser;
+import com.wicresoft.jrad.base.auth.JRadModule;
+import com.wicresoft.jrad.base.auth.JRadOperation;
+import com.wicresoft.jrad.base.constant.JRadConstants;
+import com.wicresoft.jrad.base.database.dao.common.CommonDao;
+import com.wicresoft.jrad.base.database.id.IDGenerator;
+import com.wicresoft.jrad.base.database.model.BusinessModel;
+import com.wicresoft.jrad.base.database.model.QueryResult;
+import com.wicresoft.jrad.base.i18n.I18nHelper;
+import com.wicresoft.jrad.base.web.DataBindHelper;
+import com.wicresoft.jrad.base.web.JRadModelAndView;
+import com.wicresoft.jrad.base.web.controller.BaseController;
+import com.wicresoft.jrad.base.web.result.JRadPagedQueryResult;
+import com.wicresoft.jrad.base.web.result.JRadReturnMap;
+import com.wicresoft.jrad.base.web.security.LoginManager;
+import com.wicresoft.jrad.base.web.utility.WebRequestHelper;
+import com.wicresoft.jrad.modules.dictionary.DictionaryManager;
+import com.wicresoft.jrad.modules.dictionary.model.Dictionary;
+import com.wicresoft.jrad.modules.dictionary.model.DictionaryItem;
+import com.wicresoft.jrad.modules.privilege.model.User;
+import com.wicresoft.util.spring.Beans;
+import com.wicresoft.util.spring.mvc.mv.AbstractModelAndView;
+
+@Controller
+@RequestMapping("/intopieces/intopiecesrecieve/*")
+@JRadModule("intopieces.intopiecesrecieve")
+public class IntoPiecesRecieveControl extends BaseController {
+
+	@Autowired
+	private IntoPiecesService intoPiecesService;
+
+	@Autowired
+	private CustomerInforService customerInforService;
+
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private CustomerInforService customerInforservice;
+	
+	@Autowired
+	private XM_APPLN_Service xM_APPLN_Service;
+	
+	@Autowired
+	private DataAccessSqlService dataAccessSqlService;
+
+	@Autowired
+	private CommonDao commonDao;
+
+	@Autowired
+	private CustomerInforDao customerInforDao;
+	
+	@Autowired
+	private CustomerInforCommDao customerinforcommDao;
+	
+	@Autowired
+	private NodeAuditService nodeAuditService;
+	
+	@Autowired
+	private ProcessService processService;
+	
+	@Autowired
+	private CustomerApplicationIntopieceWaitService customerApplicationIntopieceWaitService;
+	
+	@Autowired
+	private CustomerApplicationProcessService customerApplicationProcessService;
+	
+	/**
+	 * 接收进件页面
+	 * 
+	 * @param filter
+	 * @param request
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "recieve.page", method = { RequestMethod.GET })
+	public AbstractModelAndView recieve(@ModelAttribute CustomerApplicationProcessFilter filter, HttpServletRequest request) throws SQLException {
+		filter.setRequest(request);
+		IUser user = Beans.get(LoginManager.class).getLoggedInUser(request);
+		String loginId = user.getId();
+		filter.setLoginId(loginId);
+		filter.setIsReceive("YES");
+		QueryResult<CustomerApplicationIntopieceWaitForm> result = customerApplicationIntopieceWaitService.recieveIntopieceWaitForm(filter);
+		JRadPagedQueryResult<CustomerApplicationIntopieceWaitForm> pagedResult = new JRadPagedQueryResult<CustomerApplicationIntopieceWaitForm>(filter, result);
+
+		JRadModelAndView mv = new JRadModelAndView(
+				"/intopieces/intopieces_wait/intopiecesApprove_recieve", request);
+		mv.addObject(PAGED_RESULT, pagedResult);
+		mv.addObject("filter", filter);
+		return mv;
+	}
+	
+	/**
+	 * 执行接收
+	 * 
+	 * @param customerApplicationProcess
+	 * @param request
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "recieveOver.json")
+	public JRadReturnMap recieveOver(HttpServletRequest request) {
+		JRadReturnMap returnMap = new JRadReturnMap();
+			try {
+				String id = request.getParameter("id");
+				CustomerApplicationProcess process =  customerApplicationProcessService.findById(id);
+				request.setAttribute("serialNumber", process.getSerialNumber());
+				request.setAttribute("applicationId", process.getApplicationId());
+				request.setAttribute("applicationStatus", ApplicationStatusEnum.APPROVE);
+				request.setAttribute("objection", "false");
+				request.setAttribute("examineAmount", "");
+				customerApplicationIntopieceWaitService.updateCustomerApplicationProcessBySerialNumberApplicationInfo1(request);
+				returnMap.addGlobalMessage(CHANGE_SUCCESS);
+			} catch (Exception e) {
+				return WebRequestHelper.processException(e);
+			}
+		return returnMap;
+	}
+}
