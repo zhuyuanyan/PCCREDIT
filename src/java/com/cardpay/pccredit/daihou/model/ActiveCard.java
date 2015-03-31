188eab5813a179d1826199257657bda2511ab0c3
tanwh <qk_tanwh@163.com>
2015-03-31 19:06:53 +0800

    [mod]几个遗漏

--- src/java/com/cardpay/pccredit/daihou/model/ActiveCard.java
+++ src/java/com/cardpay/pccredit/daihou/model/ActiveCard.java
@@ -5,24 +5,24 @@ import com.wicresoft.jrad.base.database.model.BusinessModel;
 public class ActiveCard extends BusinessModel{
 	private static final long serialVersionUID = 1L;
 	
-	private String customer_id;
-	private String chinese_name;
+	private String customerid;
+	private String chinesename;
 	private String product;
 	private String cardid;//card_information表的id
-	private String card_number;//card_information表的CARD_NUMBER
-	private String card_activate_date;
-	private String expire_date;//到期日
-	public String getCustomer_id() {
-		return customer_id;
+	private String cardnumber;//card_information表的CARD_NUMBER
+	private String cardactivatedate;
+	private String expiredate;//到期日
+	public String getCustomerid() {
+		return customerid;
 	}
-	public void setCustomer_id(String customer_id) {
-		this.customer_id = customer_id;
+	public void setCustomerid(String customerid) {
+		this.customerid = customerid;
 	}
-	public String getChinese_name() {
-		return chinese_name;
+	public String getChinesename() {
+		return chinesename;
 	}
-	public void setChinese_name(String chinese_name) {
-		this.chinese_name = chinese_name;
+	public void setChinesename(String chinesename) {
+		this.chinesename = chinesename;
 	}
 	public String getProduct() {
 		return product;
@@ -36,23 +36,22 @@ public class ActiveCard extends BusinessModel{
 	public void setCardid(String cardid) {
 		this.cardid = cardid;
 	}
-	public String getCard_number() {
-		return card_number;
+	public String getCardnumber() {
+		return cardnumber;
 	}
-	public void setCard_number(String card_number) {
-		this.card_number = card_number;
+	public void setCardnumber(String cardnumber) {
+		this.cardnumber = cardnumber;
 	}
-	public String getCard_activate_date() {
-		return card_activate_date;
+	public String getCardactivatedate() {
+		return cardactivatedate;
 	}
-	public void setCard_activate_date(String card_activate_date) {
-		this.card_activate_date = card_activate_date;
+	public void setCardactivatedate(String cardactivatedate) {
+		this.cardactivatedate = cardactivatedate;
 	}
-	public String getExpire_date() {
-		return expire_date;
+	public String getExpiredate() {
+		return expiredate;
 	}
-	public void setExpire_date(String expire_date) {
-		this.expire_date = expire_date;
+	public void setExpiredate(String expiredate) {
+		this.expiredate = expiredate;
 	}
-	
 }
