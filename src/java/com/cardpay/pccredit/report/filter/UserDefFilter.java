25b9880ed2a7218fad0201a0148b8a08bc93f6c3
tanwh <qk_tanwh@163.com>
2015-04-14 11:50:34 +0800

    [mod]智能报表翻页问题

---
+++ src/java/com/cardpay/pccredit/report/filter/UserDefFilter.java
@@ -0,0 +1,22 @@
+package com.cardpay.pccredit.report.filter;
+
+import java.util.Date;
+
+import com.wicresoft.jrad.base.web.filter.BaseQueryFilter;
+
+/**
+ * @author chenzhifang
+ *
+ * 2014-11-27下午4:29:53
+ */
+public class UserDefFilter extends BaseQueryFilter {
+	private String userDef;
+
+	public String getUserDef() {
+		return userDef;
+	}
+
+	public void setUserDef(String userDef) {
+		this.userDef = userDef;
+	}
+}
