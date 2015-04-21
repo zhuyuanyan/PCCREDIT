1b78e3ccc3ed00f17a68fb7f0049d4834c99c02c
qkhj <qk_hejia@163.com>
2015-04-21 14:38:53 +0800

    【add】智能报表机构导出

--- src/java/com/cardpay/pccredit/report/dao/IntelligentReportDao.java
+++ src/java/com/cardpay/pccredit/report/dao/IntelligentReportDao.java
@@ -26,7 +26,7 @@ public interface IntelligentReportDao {
 	
 	List<IntelligentCustomerReport> findIntelligentCustomerReport();
 	
-	List<IntelligentAccountReport2> findIntelligentAccountReportAll(@Param("year") int year,@Param("month") int month,@Param("lastYear") int lastYear,@Param("lastMonth") int lastMonth);
+	List<IntelligentAccountReport2> findIntelligentAccountReportAll(UserDefFilter filter);
 	List<IntelligentAccountReport2> findIntelligentAccountReport(UserDefFilter filter);
 	int findIntelligentAccountReportCount();
 
--- src/java/com/cardpay/pccredit/report/filter/UserDefFilter.java
+++ src/java/com/cardpay/pccredit/report/filter/UserDefFilter.java
@@ -11,6 +11,8 @@ import com.wicresoft.jrad.base.web.filter.BaseQueryFilter;
  */
 public class UserDefFilter extends BaseQueryFilter {
 	private String userDef;
+	//选择机构
+	private String orgId;
 
 	public String getUserDef() {
 		return userDef;
@@ -20,5 +22,12 @@ public class UserDefFilter extends BaseQueryFilter {
 		this.userDef = userDef;
 	}
 
-	
+	public String getOrgId() {
+		return orgId;
+	}
+
+	public void setOrgId(String orgId) {
+		this.orgId = orgId;
+	}
+
 }
