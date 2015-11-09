5c0ea522b898fdc8d63fff6c4f02ed3819b0fdc6
songchengxian <411137909@qq.com>
2015-11-09 16:29:35 +0800

    add 调额流程管理台

---
+++ src/java/com/cardpay/pccredit/intopieces/model/XmModel.java
@@ -0,0 +1,366 @@
+package com.cardpay.pccredit.intopieces.model;
+
+import com.wicresoft.jrad.base.database.model.BusinessModel;
+import com.wicresoft.jrad.base.database.model.ModelParam;
+
+@ModelParam(table = "XM_MODEL")
+public class XmModel extends BusinessModel {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String  app_id;
+	private String  intopiecestype;            
+	private String  applyquota;                
+	private String  applyintopiecessparetype_1;
+	private String  house_type;                
+	private String  house_type1;               
+	private String  value_1;                   
+	private String  value_2;                   
+	private String  value_3;                   
+	private String  value_4;                   
+	private String  value_5;                   
+	private String  value_6;                   
+	private String  value_7;                   
+	private String  value_7_1;                 
+	private String  value_8;                   
+	private String  value_9;                  
+	private String  value_10;                  
+	private String  value_11;                  
+	private String  value_12;                  
+	private String  house_type2;               
+	private String  value_13;                  
+	private String  value_14;                  
+	private String  value_15;                 
+	private String  value_16;                  
+	private String  value_17;                  
+	private String  value_18;                  
+	private String  value_19;                  
+	private String  value_20;                  
+	private String  value_21;                  
+	private String  value_22;                  
+	private String  value_23;                  
+	private String  value_24;                  
+	private String  value_25;                  
+	private String  value_26;                  
+	private String  value_27;                  
+	private String  house_type3;               
+	private String  value_28;                  
+	private String  value_29;                  
+	private String  value_30;                  
+	private String  value_31;                  
+	private String  value_32;                  
+	private String  value_33;                  
+	private String  value_34;                  
+	private String  value_35;                  
+	private String  value_36;                  
+	private String  value_37;                  
+	private String  value_38;                  
+	private String  value_39;                  
+	private String  value_40;                  
+	private String  value_41;
+	
+	
+	public String getApp_id() {
+		return app_id;
+	}
+	public void setApp_id(String app_id) {
+		this.app_id = app_id;
+	}
+	public String getIntopiecestype() {
+		return intopiecestype;
+	}
+	public void setIntopiecestype(String intopiecestype) {
+		this.intopiecestype = intopiecestype;
+	}
+	public String getApplyquota() {
+		return applyquota;
+	}
+	public void setApplyquota(String applyquota) {
+		this.applyquota = applyquota;
+	}
+	public String getApplyintopiecessparetype_1() {
+		return applyintopiecessparetype_1;
+	}
+	public void setApplyintopiecessparetype_1(String applyintopiecessparetype_1) {
+		this.applyintopiecessparetype_1 = applyintopiecessparetype_1;
+	}
+	public String getHouse_type() {
+		return house_type;
+	}
+	public void setHouse_type(String house_type) {
+		this.house_type = house_type;
+	}
+	public String getHouse_type1() {
+		return house_type1;
+	}
+	public void setHouse_type1(String house_type1) {
+		this.house_type1 = house_type1;
+	}
+	public String getValue_1() {
+		return value_1;
+	}
+	public void setValue_1(String value_1) {
+		this.value_1 = value_1;
+	}
+	public String getValue_2() {
+		return value_2;
+	}
+	public void setValue_2(String value_2) {
+		this.value_2 = value_2;
+	}
+	public String getValue_3() {
+		return value_3;
+	}
+	public void setValue_3(String value_3) {
+		this.value_3 = value_3;
+	}
+	public String getValue_4() {
+		return value_4;
+	}
+	public void setValue_4(String value_4) {
+		this.value_4 = value_4;
+	}
+	public String getValue_5() {
+		return value_5;
+	}
+	public void setValue_5(String value_5) {
+		this.value_5 = value_5;
+	}
+	public String getValue_6() {
+		return value_6;
+	}
+	public void setValue_6(String value_6) {
+		this.value_6 = value_6;
+	}
+	public String getValue_7() {
+		return value_7;
+	}
+	public void setValue_7(String value_7) {
+		this.value_7 = value_7;
+	}
+	public String getValue_7_1() {
+		return value_7_1;
+	}
+	public void setValue_7_1(String value_7_1) {
+		this.value_7_1 = value_7_1;
+	}
+	public String getValue_8() {
+		return value_8;
+	}
+	public void setValue_8(String value_8) {
+		this.value_8 = value_8;
+	}
+	public String getValue_9() {
+		return value_9;
+	}
+	public void setValue_9(String value_9) {
+		this.value_9 = value_9;
+	}
+	public String getValue_10() {
+		return value_10;
+	}
+	public void setValue_10(String value_10) {
+		this.value_10 = value_10;
+	}
+	public String getValue_11() {
+		return value_11;
+	}
+	public void setValue_11(String value_11) {
+		this.value_11 = value_11;
+	}
+	public String getValue_12() {
+		return value_12;
+	}
+	public void setValue_12(String value_12) {
+		this.value_12 = value_12;
+	}
+	public String getHouse_type2() {
+		return house_type2;
+	}
+	public void setHouse_type2(String house_type2) {
+		this.house_type2 = house_type2;
+	}
+	public String getValue_13() {
+		return value_13;
+	}
+	public void setValue_13(String value_13) {
+		this.value_13 = value_13;
+	}
+	public String getValue_14() {
+		return value_14;
+	}
+	public void setValue_14(String value_14) {
+		this.value_14 = value_14;
+	}
+	public String getValue_15() {
+		return value_15;
+	}
+	public void setValue_15(String value_15) {
+		this.value_15 = value_15;
+	}
+	public String getValue_16() {
+		return value_16;
+	}
+	public void setValue_16(String value_16) {
+		this.value_16 = value_16;
+	}
+	public String getValue_17() {
+		return value_17;
+	}
+	public void setValue_17(String value_17) {
+		this.value_17 = value_17;
+	}
+	public String getValue_18() {
+		return value_18;
+	}
+	public void setValue_18(String value_18) {
+		this.value_18 = value_18;
+	}
+	public String getValue_19() {
+		return value_19;
+	}
+	public void setValue_19(String value_19) {
+		this.value_19 = value_19;
+	}
+	public String getValue_20() {
+		return value_20;
+	}
+	public void setValue_20(String value_20) {
+		this.value_20 = value_20;
+	}
+	public String getValue_21() {
+		return value_21;
+	}
+	public void setValue_21(String value_21) {
+		this.value_21 = value_21;
+	}
+	public String getValue_22() {
+		return value_22;
+	}
+	public void setValue_22(String value_22) {
+		this.value_22 = value_22;
+	}
+	public String getValue_23() {
+		return value_23;
+	}
+	public void setValue_23(String value_23) {
+		this.value_23 = value_23;
+	}
+	public String getValue_24() {
+		return value_24;
+	}
+	public void setValue_24(String value_24) {
+		this.value_24 = value_24;
+	}
+	public String getValue_25() {
+		return value_25;
+	}
+	public void setValue_25(String value_25) {
+		this.value_25 = value_25;
+	}
+	public String getValue_26() {
+		return value_26;
+	}
+	public void setValue_26(String value_26) {
+		this.value_26 = value_26;
+	}
+	public String getValue_27() {
+		return value_27;
+	}
+	public void setValue_27(String value_27) {
+		this.value_27 = value_27;
+	}
+	public String getHouse_type3() {
+		return house_type3;
+	}
+	public void setHouse_type3(String house_type3) {
+		this.house_type3 = house_type3;
+	}
+	public String getValue_28() {
+		return value_28;
+	}
+	public void setValue_28(String value_28) {
+		this.value_28 = value_28;
+	}
+	public String getValue_29() {
+		return value_29;
+	}
+	public void setValue_29(String value_29) {
+		this.value_29 = value_29;
+	}
+	public String getValue_30() {
+		return value_30;
+	}
+	public void setValue_30(String value_30) {
+		this.value_30 = value_30;
+	}
+	public String getValue_31() {
+		return value_31;
+	}
+	public void setValue_31(String value_31) {
+		this.value_31 = value_31;
+	}
+	public String getValue_32() {
+		return value_32;
+	}
+	public void setValue_32(String value_32) {
+		this.value_32 = value_32;
+	}
+	public String getValue_33() {
+		return value_33;
+	}
+	public void setValue_33(String value_33) {
+		this.value_33 = value_33;
+	}
+	public String getValue_34() {
+		return value_34;
+	}
+	public void setValue_34(String value_34) {
+		this.value_34 = value_34;
+	}
+	public String getValue_35() {
+		return value_35;
+	}
+	public void setValue_35(String value_35) {
+		this.value_35 = value_35;
+	}
+	public String getValue_36() {
+		return value_36;
+	}
+	public void setValue_36(String value_36) {
+		this.value_36 = value_36;
+	}
+	public String getValue_37() {
+		return value_37;
+	}
+	public void setValue_37(String value_37) {
+		this.value_37 = value_37;
+	}
+	public String getValue_38() {
+		return value_38;
+	}
+	public void setValue_38(String value_38) {
+		this.value_38 = value_38;
+	}
+	public String getValue_39() {
+		return value_39;
+	}
+	public void setValue_39(String value_39) {
+		this.value_39 = value_39;
+	}
+	public String getValue_40() {
+		return value_40;
+	}
+	public void setValue_40(String value_40) {
+		this.value_40 = value_40;
+	}
+	public String getValue_41() {
+		return value_41;
+	}
+	public void setValue_41(String value_41) {
+		this.value_41 = value_41;
+	}                  
+	
+	
+	
+}
