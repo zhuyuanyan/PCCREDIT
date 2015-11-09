e2e370f470dcff06ea8e2b0280b7328879430296
songchengxian <411137909@qq.com>
2015-11-09 16:25:03 +0800

    add 查询进件详情调查模板显示

--- src/java/com/cardpay/pccredit/intopieces/dao/LocalExcelDao.java
+++ src/java/com/cardpay/pccredit/intopieces/dao/LocalExcelDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.cardpay.pccredit.intopieces.filter.AddIntoPiecesFilter;
 import com.cardpay.pccredit.intopieces.model.LocalExcel;
+import com.cardpay.pccredit.intopieces.model.XmModel;
 import com.cardpay.pccredit.intopieces.web.LocalExcelForm;
 import com.wicresoft.util.annotation.Mapper;
 
@@ -15,5 +16,6 @@ public interface LocalExcelDao {
 	public LocalExcel findById(@Param("id") String id);
 	public int findCountByProductAndCustomer(AddIntoPiecesFilter filter);
 	public LocalExcel findByApplication(@Param("applicationId") String applicationId);
+	public XmModel findModelById(@Param("applicationId") String applicationId);
 	
 }
